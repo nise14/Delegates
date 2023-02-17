1b76258e3c239c2f26fa866c4efe8e0d88a81338
Nicolas <nise14@htomail.com>
2023-02-17 12:41:02 -0500

    Add action

--- Delegates/Program.cs
+++ Delegates/Program.cs
@@ -1,5 +1,6 @@
 ﻿using Delegates;
 
+#region Delegates Basic
 Console.WriteLine("Delegates!!");
 
 var person1 = new Person
@@ -39,4 +40,73 @@ bool AgeGreather20(int age)
     return age > 20;
 }
 
+#endregion
+
+Console.WriteLine();
+
+#region Actions
+Console.WriteLine("Actions");
+
+Action action1 = delegate ()
+{
+    Console.WriteLine("Hello world!");
+};
+
+action1();
+
+Action<string> action2 = delegate (string name)
+{
+    var reverse = name.ToArray();
+    Array.Reverse(reverse);
+    Console.WriteLine(new string(reverse));
+};
+
+action2("Nicolas");
+
+Action<string> action3 = (string name) =>
+{
+    var reverse = name.ToArray();
+    Array.Reverse(reverse);
+    Console.WriteLine(new string(reverse));
+};
+
+action3("Nicks");
+
+Action<string> action4 = (name) =>
+{
+    var reverse = name.ToArray();
+    Array.Reverse(reverse);
+    Console.WriteLine(new string(reverse));
+};
+
+action4("anita lava la tina");
+
+void ReverseText(string text)
+{
+    var reverse = text.ToCharArray();
+    Array.Reverse(reverse);
+    Console.WriteLine(new string(reverse));
+}
+
+Action<string> action5 = name => ReverseText(name);
+
+action3("Oro");
+
+Action<int, int> action6 = (x, y) =>
+{
+    var sum = x + y;
+    var text = sum switch
+    {
+        < 0 => "Suma negativa",
+        > 0 => "Suma positiva",
+        _ => "Valor cero"
+    };
+
+    Console.WriteLine(text);
+};
+
+action6(0, -1);
+#endregion
+
 public delegate bool AgesGreatherTo20(int age);
+
